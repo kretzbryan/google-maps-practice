aeea9f5a5adc698f329e3530103c4c77372aa8dc
bryan kretz <kretzbryan@gmail.com>
2021-02-01 13:56:40 -0800

    Company class created.

--- src/Company.ts
+++ src/Company.ts
@@ -1,1 +1,18 @@
-class Company {}
+import faker from 'faker';
+
+export class Company {
+	name: string;
+	catchPhrase: string;
+	location: {
+		lat: number;
+		long: number;
+	};
+	constructor() {
+		this.name = faker.company.companyName();
+		this.catchPhrase = faker.company.catchPhrase();
+		this.location = {
+			lat: parseFloat(faker.address.latitude()),
+			long: parseFloat(faker.address.longitude()),
+		};
+	}
+}
