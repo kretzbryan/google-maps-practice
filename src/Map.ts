5d1d1fab170b3cd8c4503a9b2e18524ca3c4873c
bryan kretz <kretzbryan@gmail.com>
2021-03-06 20:56:52 -0800

    Map class created with google maps API

--- src/Map.ts
+++ src/Map.ts
@@ -1,1 +1,13 @@
-class Map {}
+export class Map {
+	private googleMap: google.maps.Map;
+
+	constructor() {
+		this.googleMap = new google.maps.Map(document.getElementById('map'), {
+			zoom: 1,
+			center: {
+				lat: 0,
+				lng: 0,
+			},
+		});
+	}
+}
